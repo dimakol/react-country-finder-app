ba3459370af17778558aa32db8414235df44676d
Dima Kolyas <dimakol@gmail.com>
2022-05-29 06:16:57 +0300

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/country-finder/",
   plugins: [react()],
   define: {
     "process.env": process.env,
