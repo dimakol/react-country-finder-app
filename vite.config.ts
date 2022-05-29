1e07217cdd38b39ca51406de3278293e1a761746
Dima Kolyas <dimakol@gmail.com>
2022-05-29 08:01:28 +0300

    comment base in vite.config.ts for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/country-finder/",
+  // when needing to go into dev mode, comment base
+  //base: "/country-finder/",
   plugins: [react()],
   define: {
     "process.env": process.env,
