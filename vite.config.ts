4ad99075a38091e968104dcfb55407318189c649
Dima Kolyas <dimakol@gmail.com>
2022-05-30 17:02:31 +0300

    refactor(vite.config.ts): remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // when needing to go into dev mode, comment base
   base: "/country-finder/",
   plugins: [react()],
 });
