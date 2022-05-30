65b66bb9eef01893835ec1c7d717eae7e7c9ed37
Dima Kolyas <dimakol@gmail.com>
2022-05-30 18:20:30 +0300

    fix(Sidebar): closeSideBar on link click

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import "./Sidebar.scss";
 
 const Sidebar: React.FC<{ items: string[] }> = ({ items }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleIsOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const closeSideBar = () => {
+    setOpen(false);
+  };
+
   return (
-    <Menu>
+    <Menu isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}>
       {items.map((item: string, index: number) => (
         <div key={index.toString()}>
-          <Link className="menu-item" to={`${item}`}>
+          <Link className="menu-item" to={`${item}`} onClick={closeSideBar}>
             {item}
           </Link>
         </div>
